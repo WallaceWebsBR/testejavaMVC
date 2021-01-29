5c52b55731d3380e95ca9cfe5652fe85a7b13732
Wallace Alberto <1320wallace1320@outlook.com>
2021-01-29 15:59:41 -0300

    Arquivo com tabelas do Banco

---
+++ localhost.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 29, 2021 at 06:58 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `processodb`
+--
+CREATE DATABASE IF NOT EXISTS `processodb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `processodb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `processos`
+--
+
+CREATE TABLE `processos` (
+  `id` int(11) NOT NULL,
+  `tipoprocesso` text,
+  `nomeprocesso` text,
+  `numeroprocesso` text,
+  `dataentrada` date DEFAULT NULL,
+  `valorrecurso` varchar(500) DEFAULT NULL,
+  `objetivo` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tiposprocesso`
+--
+
+CREATE TABLE `tiposprocesso` (
+  `id` int(11) NOT NULL,
+  `tipoprocesso` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `processos`
+--
+ALTER TABLE `processos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tiposprocesso`
+--
+ALTER TABLE `tiposprocesso`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `processos`
+--
+ALTER TABLE `processos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `tiposprocesso`
+--
+ALTER TABLE `tiposprocesso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
