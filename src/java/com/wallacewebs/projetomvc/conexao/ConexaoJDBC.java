eea4a6a4d93f3f64a3fc5810c7a7791424bfa845
Wallace Alberto <1320wallace1320@outlook.com>
2021-02-02 10:43:07 -0300

    fix sql dont close connection

--- src/java/com/wallacewebs/projetomvc/conexao/ConexaoJDBC.java
+++ src/java/com/wallacewebs/projetomvc/conexao/ConexaoJDBC.java
@@ -5,7 +5,7 @@
  */
 package com.wallacewebs.projetomvc.conexao;
 
-import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Driver;
 import java.sql.*;
 /**
  *
@@ -17,7 +17,7 @@ public class ConexaoJDBC {
     private void conectar() throws SQLException, SQLException, ClassNotFoundException{
         System.out.println("Conectando com o Banco de Dados...");
             Class.forName("com.mysql.jdbc.Driver");
-            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/processodb", "root", "root");
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://remotemysql.com/7LGovYAoXW", "7LGovYAoXW", "kyrhiPIsV3");
     }
     
     public Connection getConexao() throws SQLException, ClassNotFoundException{
